2fa33550a717a334c89761ae1003f5a10adf6aa7
samuelc621 <sam.corso@newtonx.com>
2020-11-16 19:04:47 -0500

    parsing numerics coming from DB as strings to floats

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const stocks = require("./core/stocks");
 const redis = require("./services/redis");
 
+const types = require("pg").types;
+types.setTypeParser(1700, val => {
+  return parseFloat(val);
+});
+
 stocks.loadStocks();
 
 process.on("SIGINT", () => {
