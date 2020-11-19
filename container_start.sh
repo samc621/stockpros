d84e631547d3b3fd1a47068c3dbbdd70b098d0ad
samuelc621 <sam.corso@newtonx.com>
2020-11-19 18:18:59 -0500

    adding unit tests to docker startup

--- container_start.sh
+++ container_start.sh
@@ -6,5 +6,7 @@ echo "running DB migration scripts"
 npm run knex migrate:latest
 echo "running DB seeder scripts"
 npm run knex seed:run
+echo "running unit tests"
+npm test
 echo "starting node server"
 npm start
