bb676635043545da0ee056d264661d22c2e9d2e8
samuelc621 <sam.corso@newtonx.com>
2021-01-19 22:48:14 -0500

    Fixing seeders and adding one for watched_symbols

--- db/seeds/strategies.js
+++ db/seeds/strategies.js
@@ -1,10 +1,15 @@
 exports.seed = function (knex) {
-  return knex("strategies").insert({
-    id: 1,
-    name: "Strategy 1",
-    buy_percentage: 0.7,
-    buy_percentage_type_of: "buying_power",
-    sell_percentage: 1,
-    sell_percentage_type_of: "qty"
-  });
+  return knex("strategy_signals").del().then(function () {
+    return knex('strategies').del()
+      .then(function () {
+        return knex("strategies").insert({
+          id: 1,
+          name: "Strategy 1",
+          buy_percentage: 0.7,
+          buy_percentage_type_of: "buying_power",
+          sell_percentage: 1,
+          sell_percentage_type_of: "qty"
+        });
+      })
+  })
 };
